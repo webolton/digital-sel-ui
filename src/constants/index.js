0f2087a24adc57bfbc7a0a36cbc62eeb5f2ce702
William Bolton <william.ellet@gmail.com>
2018-10-21 09:32:29 -0400

    Add alert reducer and constants

---
+++ src/constants/index.js
@@ -0,0 +1,1 @@
+export { default as alertConstants } from './alert.constants';
---
+++ src/reducers/index.js
@@ -0,0 +1,16 @@
+import { combineReducers } from 'redux';
+import home from './home';
+import { authentication } from './authentication.reducer';
+import { users } from './users.reducer';
+import { user } from './user.reducer';
+import alert from './alert.reducer';
+
+const rootReducer = combineReducers({
+  authentication,
+  users,
+  user,
+  alert,
+  home,
+});
+
+export default rootReducer;
