8124027560879fbb203e375d2f18520782e16b6a
William Bolton <william.ellet@gmail.com>
2018-11-25 08:57:53 -0500

    Add route to ShowUserPage

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import isNil from 'lodash/isNil';
 import HomePage from 'views/HomePage';
 import LoginPage from 'views/users/LoginPage';
 import LogoutPage from 'views/users/LogoutPage';
+import ShowUserPage from 'views/users/ShowUserPage';
 import NotFoundPage from 'views/Errors/NotFoundPage';
+import PrivateRoute from './PrivateRoute';
 
 class Routes extends React.Component {
   render() {
@@ -13,6 +16,7 @@ class Routes extends React.Component {
         <Route exact path="/" component={HomePage} name="HomePage" />
         <Route exact path="/login" component={LoginPage} />
         <Route exact path="/logout" component={LogoutPage} />
+        <PrivateRoute exact path="/users/:id" component={ShowUserPage} isLoggedIn={!isNil(currentUser)} />
         <Route component={NotFoundPage} />
       </Switch>
     );
