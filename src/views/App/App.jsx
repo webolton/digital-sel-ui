cb58d0de6540a6ac50b7fc2857c16499dc18b4f4
William Bolton <william.ellet@gmail.com>
2018-12-02 14:27:15 -0500

    Add animated mounting

--- src/views/App/App.jsx
+++ src/views/App/App.jsx
@@ -9,39 +9,74 @@ import withStyles from '@material-ui/core/styles/withStyles';
 import headerStyle from 'assets/javascripts/components/headerStyle';
 import Header from 'components/Header';
 import HeaderLinks from 'components/Header/HeaderLinks';
+import Transition from 'react-transition-group/Transition';
 import { alertActions } from 'actions';
+
+const duration = 300;
+
+const defaultStyle = {
+  transition: `opacity ${duration}ms ease-in-out`,
+  opacity: 0,
+};
+
+const transitionStyles = {
+  entering: { opacity: 0 },
+  entered: { opacity: 1 },
+};
 
 export class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      inProp: false,
+    };
     const { dispatch } = this.props;
     const unlisten = history.listen((location, action) => {
       dispatch(alertActions.clear());
     });
 
     if (window.performance) {
-      if (performance.navigation.type == 1) {
+      if (performance.navigation.type === 1) {
         dispatch(alertActions.clear());
       }
     }
   }
 
+  componentDidMount() {
+    this.setState({ inProp: true });
+  }
+
   render() {
     const dashboardRoutes = [];
-    const { classes, currentUser, ...rest } = this.props;
+    const { inProp } = this.state;
+    const {
+      classes, currentUser, ...rest
+    } = this.props;
     return (
-      <div>
-        <Header
-          color="lightGray"
-          routes={dashboardRoutes}
-          brand="The Digital South English Legendary"
-          rightLinks={<HeaderLinks />}
-          absolute
-          changeColorOnScroll="false"
-          {...rest}
-        />
-        <Routes currentUser={currentUser} />
-      </div>
+      <Transition
+        in={inProp}
+        timeout={300}
+        unmountOnExit
+      >
+        {state => (
+          <div style={{
+            ...defaultStyle,
+            ...transitionStyles[state],
+          }}
+          >
+            <Header
+              color="lightGray"
+              routes={dashboardRoutes}
+              brand="The Digital South English Legendary"
+              rightLinks={<HeaderLinks />}
+              absolute
+              changeColorOnScroll="false"
+              {...rest}
+            />
+            <Routes currentUser={currentUser} />
+          </div>
+        )}
+      </Transition>
     );
   }
 }
