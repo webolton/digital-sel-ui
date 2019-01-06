b832da006182843d25828a76bb38224f40245e01
William Bolton <william.ellet@gmail.com>
2019-01-06 16:27:13 -0500

    Send data type in request header

--- src/utilities/authHeader.js
+++ src/utilities/authHeader.js
@@ -3,7 +3,10 @@ const authHeader = () => {
   const currentUser = JSON.parse(localStorage.getItem('currentUser'));
 
   if (currentUser && currentUser.token) {
-    return { Authorization: currentUser.token };
+    return {
+      Authorization: currentUser.token,
+      'Content-Type': 'application/json',
+    };
   }
   return {};
 };
