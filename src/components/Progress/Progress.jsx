a00b3ff15d1eb47854577cec155f14ad9814fa40
William Bolton <william.ellet@gmail.com>
2019-01-06 16:26:16 -0500

    Wrap progress bar in container

--- src/components/Progress/Progress.jsx
+++ src/components/Progress/Progress.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import LinearProgress from '@material-ui/core/LinearProgress';
-import { darkCerulean } from 'assets/javascripts/digital-sel-ui';
+import { darkCerulean, container } from 'assets/javascripts/digital-sel-ui';
 
 const styles = {
+  container: {
+    ...container,
+    zIndex: '2',
+    position: 'relative',
+    paddingTop: '20vh',
+    color: '#FFFFFF',
+  },
   root: {
     marginTop: '80px',
     textAlign: 'center',
@@ -18,16 +25,26 @@ const styles = {
 };
 
 function Progress(props) {
-  const { classes } = props;
+  const { classes, message } = props;
   return (
-    <div className={classes.root}>
-      <LinearProgress
-        classes={{
-          colorPrimary: classes.linearColorPrimary,
-          barColorPrimary: classes.linearBarColorPrimary,
-        }}
-      />
-      <h2>Loading</h2>
+    <div
+      className={classes.pageHeader}
+      style={{
+        backgroundSize: 'cover',
+        backgroundPosition: 'top center',
+      }}
+    >
+      <div className={classes.container}>
+        <div className={classes.root}>
+          <LinearProgress
+            classes={{
+              colorPrimary: classes.linearColorPrimary,
+              barColorPrimary: classes.linearBarColorPrimary,
+            }}
+          />
+          <h2>{message}</h2>
+        </div>
+      </div>
     </div>
   );
 }
