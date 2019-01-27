247ea489f8de656ea061e1eb3defea2880ad4bfa
William Bolton <william.ellet@gmail.com>
2019-01-27 12:17:55 -0500

    Add deploy scripts

---
+++ config/deploy.rb
@@ -0,0 +1,30 @@
+lock '~> 3.11.0'
+
+set :application,     'digital-sel-ui'
+set :repo_url,        'git@github.com:webolton/digital-sel-ui.git'
+set :deploy_to,       '/var/www/digital-sel-ui'
+
+append :linked_files, '.env'
+append :linked_dirs,  'node_modules'
+
+set :user,            'deploy'
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+
+set :nvm_type,        :user
+set :nvm_node,        'v10.13.0'
+set :nvm_map_bins,    %w{node npm yarn}
+
+set :yarn_flags,      %w{--silent --no-progress}
+
+namespace :deploy do
+
+  task :yarn_deploy do
+    on roles fetch(:yarn_roles) do
+      within fetch(:yarn_target_path, release_path) do
+        execute fetch(:yarn_bin), 'build'
+      end
+    end
+  end
+
+  before 'symlink:release', :yarn_deploy
+end
