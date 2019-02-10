0b0222bbccac3bbb10c36c2a982f37328a08c2b9
William Bolton <william.ellet@gmail.com>
2019-02-10 12:44:19 -0500

    Improve linting

--- src/components/Grid/GridContainer.jsx
+++ src/components/Grid/GridContainer.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
-// nodejs library to set properties for components
 import PropTypes from 'prop-types';
-
-// @material-ui/core components
 import withStyles from '@material-ui/core/styles/withStyles';
 import Grid from '@material-ui/core/Grid';
 
@@ -31,7 +28,7 @@ GridContainer.defaultProps = {
 
 GridContainer.propTypes = {
   classes: PropTypes.object.isRequired,
-  children: PropTypes.node,
+  children: PropTypes.node.isRequired,
   className: PropTypes.string,
 };
 
