cb80544f9e94b4bfc056e3a2587de60ee418464c
William Bolton <william.ellet@gmail.com>
2019-02-16 18:21:53 -0500

    Refactor instructions element as card

--- src/views/Dashboard/DashboardInstructions.jsx
+++ src/views/Dashboard/DashboardInstructions.jsx
@@ -5,6 +5,10 @@ import { container } from 'assets/javascripts/digital-sel-ui';
 import GridContainer from 'components/Grid/GridContainer';
 import GridItem from 'components/Grid/GridItem';
 import Paper from '@material-ui/core/Paper';
+import Card from 'components/Card';
+import CardBody from 'components/Card/CardBody';
+import CardHeader from 'components/Card/CardHeader';
+import CardFooter from 'components/Card/CardFooter';
 import Typography from '@material-ui/core/Typography';
 import Fade from '@material-ui/core/Fade';
 
@@ -15,7 +19,7 @@ const styles = {
     zIndex: 2,
   },
   container,
-  ctaWrapper: {
+  instructionWrapper: {
     margin: 15,
   },
 };
@@ -27,15 +31,32 @@ function DashboardInstructions(props) {
     <div className={classes.container}>
       <Fade in={!showInstructions}>
         <GridContainer>
-          <GridItem xs={12} sm={12} md={12} lg={12} className={classes.ctaWrapper}>
-            <Paper className={classes.root} elevation={1}>
-              <Typography variant="h5" component="h3">
-              This will be the dashboard instructions.
-              </Typography>
-              <Typography component="p">
-              This will be the dashboard instructions.
-              </Typography>
-            </Paper>
+          <GridItem xs={12} sm={12} md={12} lg={12} className={classes.instructionWrapper}>
+            <Card>
+              <CardHeader color="primary" className={classes.cardHeader}>
+                <Typography
+                  variant="h5"
+                  color="inherit"
+                  align="center"
+                  >
+                  SEL Text Interaction Dashboard
+                </Typography>
+              </CardHeader>
+              <CardBody>
+                <Typography variant="h5">
+                  Dashboard instructions
+                </Typography>
+                <Typography variant="body1">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </Typography>
+              </CardBody>
+              <CardFooter/>
+            </Card>
           </GridItem>
         </GridContainer>
       </Fade>
